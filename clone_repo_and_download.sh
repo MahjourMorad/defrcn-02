81950107666535baacf471fd632be356951af56c
MahjourMorad <moradmahjour@gmail.com>
2026-02-27 01:16:13 +0000

    Initial commit

---
+++ clone_repo_and_download.sh
@@ -0,0 +1,18 @@
+pip install gdown
+https://github.com/er-muyue/DeFRCN.git
+git clone https://github.com/er-muyue/DeFRCN.git
+cd DeFRCN
+cd datasets
+apt update
+apt install unzip
+gdown --fuzzy --no-check-certificate https://drive.google.com/file/d/1BcuJ9j9Mtymp56qGSOfYxlXN4uEVyxFm/view
+gdown --fuzzy --no-check-certificate https://drive.google.com/file/d/1NjztPltqm-Z-pG94a6PiPVP4BgD8Sz1H/view
+gdown --fuzzy --no-check-certificate https://drive.google.com/file/d/1BpDDqJ0p-fQAFN_pthn2gqiK5nWGJ-1a/view
+
+unzip VOC2007.zip
+unzip VOC2012.zip
+unzip vocsplit.zip
+
+
+gdown --fuzzy --no-check-certificate https://drive.google.com/file/d/1rsE20_fSkYeIhFaNU04rBfEDkMENLibj/view
+unzip ImageNetPretrained.zip 
---
+++ setup.sh
@@ -0,0 +1,7 @@
+#git clone --branch v0.3 https://github.com/facebookresearch/detectron2.git
+cd detectron2
+pip install -e .
+cd ..
+pip install opencv-python
+pip install scikit-learn
+echo "Installation complete!"
